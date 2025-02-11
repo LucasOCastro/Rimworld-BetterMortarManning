02b313e756edaeb9210fac49dd79e9036fda657f
lucas castro <lucas_o__castro@hotmail.com>
2025-02-10 23:05:50 -0300

    Fixes CompBetterMortar and wrong name in xml

--- Source/BetterMortarManning/CompBetterMortar.cs
+++ Source/BetterMortarManning/CompBetterMortar.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using RimWorld;
 using Verse;
-using Verse.AI;
 
 // ReSharper disable InconsistentNaming
 
@@ -9,22 +8,7 @@ namespace BetterMortarManning
 {
     public class CompBetterMortar : ThingComp
     {
-        private static readonly CachedTexture ToggleTurretIcon = new CachedTexture("UI/Gizmos/ToggleTurret");
-
-        private CompMannable _mannable;
-        private CompMannable Mannable
-        {
-            get
-            {
-                if (_mannable != null)
-                    return _mannable;
-                
-                if (!parent.TryGetComp(out _mannable))
-                    throw new System.Exception($"{typeof(CompBetterMortar).FullName} requires {nameof(CompMannable)}. This comp should be added automatically only, and not manually.");
-                
-                return _mannable;
-            }
-        }
+        private static readonly CachedTexture ToggleTurretIcon = new("UI/Gizmos/ToggleTurret");
         
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
@@ -33,34 +17,14 @@ namespace BetterMortarManning
 
             if (parent.Faction != Faction.OfPlayer)
                 yield break;
-            
+
             yield return new Command_Action
             {
-                icon = ToggleTurretIcon,
-                defaultLabel = "CrazyMalk_CommandManAll",
-                defaultDesc = "CrazyMalk_CommandManAllDesc",
-                action = ManAll
+                icon = ToggleTurretIcon.Texture,
+                defaultLabel = "CrazyMalk_CommandManAll".Translate(),
+                defaultDesc = "CrazyMalk_CommandManAllDesc".Translate(),
+                action = MortarManningPlanner.ManSelected
             };
-        }
-
-        private void ManAll()
-        {
-            IReadOnlyList<Pawn> allPawnsSpawned = parent.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer);
-            allPawnsSpawned.
-        }
-
-        private bool CanMan(Pawn pawn)
-        {
-            if (!pawn.RaceProps.ToolUser)
-                return false;
-            
-            if (!pawn.CanReserveAndReach(parent, PathEndMode.InteractionCell, Danger.Deadly))
-                return false;
-                
-            if (Mannable.Props.manWorkType != WorkTags.None && pawn.WorkTagIsDisabled(Mannable.Props.manWorkType))
-                return false;
-                
-            return true;
         }
     }
 }
